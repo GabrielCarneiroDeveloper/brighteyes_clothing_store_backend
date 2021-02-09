ad4581e1bf7bc735707942f36cccf13aaa9397f8
Gabriel Carneiro <carneiro.development@gmail.com>
2021-02-08 20:05:25 -0400

    Add rule to avoid update or create client with already existent cpf

--- src/modules/client/ClientController.ts
+++ src/modules/client/ClientController.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 
 import { Client } from './../../modules/client/Client'
 import { IController } from './../../interfaces/IControllers'
 import { DTOController } from './../../common/dto/DTOController'
 import { AbstractController } from '../abstract.controller'
+import { getRepository } from 'typeorm'
 
 export class ClientController extends AbstractController implements IController {
   route: Router
@@ -22,5 +23,55 @@ export class ClientController extends AbstractController implements IController 
     this.route.get('/clients/:id', this.listOneById)
     this.route.put('/clients/:id', this.update)
     this.route.delete('/clients/:id', this.remove)
+  }
+
+  create = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const data = request.body
+      const repository = getRepository(this.ModelClassName)
+
+      const clientFound = repository.findOne({ where: { cpf: data.cpf } })
+      if (clientFound) {
+        throw new Error('Client already exists')
+      }
+
+      const createdObject = await repository.save(data, this.saveOptions)
+      return response.json({
+        message: 'Object created',
+        data: createdObject
+      })
+    } catch (error) {
+      console.error(error)
+      return response.status(401).json({
+        message: 'An error occurred',
+        error_message: error.message
+      })
+    }
+  }
+
+  update = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const data = request.body
+      const id = request.params.id
+      const repository = getRepository(this.ModelClassName)
+
+      const clientFound = (await repository.findOne({ where: { cpf: data.cpf } })) as Client
+      if (clientFound.name !== data.name) {
+        throw new Error('Client already exists')
+      }
+
+      await repository.update(id, data)
+      const updateObject = await repository.findOneOrFail(id, this.findOneOptions)
+      return response.json({
+        message: 'Object updated',
+        data: updateObject
+      })
+    } catch (error) {
+      console.error(error)
+      return response.status(401).json({
+        message: 'An error occurred',
+        error_message: error.message
+      })
+    }
   }
 }
