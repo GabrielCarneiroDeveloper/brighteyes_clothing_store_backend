31df676dc47210b5f5668062a024d9d3bcb479ec
Gabriel Carneiro <carneiro.development@gmail.com>
2021-02-08 21:30:25 -0400

    Add rule to avoid create or update clothes with already existent name

--- src/modules/clothes/ClothesController.ts
+++ src/modules/clothes/ClothesController.ts
@@ -6,6 +6,7 @@ import { AbstractController } from '../abstract.controller'
 import { Clothes } from './Clothes'
 
 import { storage } from './../../common/storage/storage'
+import { getRepository } from 'typeorm'
 
 const upload = storage('clothes')
 
@@ -27,6 +28,57 @@ export class ClothesController extends AbstractController implements IController
     this.route.put('/clothes/:id', this.update)
     this.route.delete('/clothes/:id', this.remove)
     this.route.post('/clothes/upload', upload.single('image'), this.imagesUpload)
+  }
+
+  create = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const data = request.body
+      const repository = getRepository(this.ModelClassName)
+
+      const foundClothes = (await repository.findOne({ where: { name: data.name } })) as Clothes
+      if (foundClothes) {
+        throw new Error('Clothes already registered')
+      }
+
+      const createdObject = await repository.save(data, this.saveOptions)
+      return response.json({
+        message: 'Object created',
+        data: createdObject
+      })
+    } catch (error) {
+      console.error(error)
+      return response.status(401).json({
+        message: 'An error occurred',
+        error_message: error.message
+      })
+    }
+  }
+
+  update = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const data = request.body
+      const id = request.params.id
+      const repository = getRepository(this.ModelClassName)
+
+      const foundClothes = (await repository.findOne({ where: { name: data.name } })) as Clothes
+      console.log(data.name)
+      if (foundClothes) {
+        throw new Error('Clothes already registered')
+      }
+
+      await repository.update(id, data)
+      const updateObject = await repository.findOneOrFail(id, this.findOneOptions)
+      return response.json({
+        message: 'Object updated',
+        data: updateObject
+      })
+    } catch (error) {
+      console.error(error)
+      return response.status(401).json({
+        message: 'An error occurred',
+        error_message: error.message
+      })
+    }
   }
 
   imagesUpload = async (
