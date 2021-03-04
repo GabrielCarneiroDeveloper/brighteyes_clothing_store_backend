8c424615055954b8781f565217dd06b8cd3d21cc
Gabriel Carneiro <carneiro.development@gmail.com>
2021-03-04 02:08:56 -0400

    Build v3.4

--- dist/common/database/Db.js
+++ dist/common/database/Db.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Db = void 0;
-const path_1 = __importDefault(require("path"));
 const typeorm_1 = require("typeorm");
 const logger_1 = __importDefault(require("../logger/logger"));
 const Employee_1 = require("./../../modules/employee/Employee");
@@ -15,14 +14,19 @@ const EmployeeClientStatus_1 = require("./../../modules/employee_client_status/E
 const EmployeeTitle_1 = require("./../../modules/employee_title/EmployeeTitle");
 const ShoppingCart_1 = require("./../../modules/shopping-cart/ShoppingCart");
 const ShoppingCartStatus_1 = require("./../../modules/shopping_cart_status/ShoppingCartStatus");
+const app_config_1 = __importDefault(require("@src/config/app.config"));
 class Db {
     async init() {
         try {
             this.instance = await typeorm_1.createConnection({
                 synchronize: true,
                 logging: false,
-                type: 'sqlite',
-                database: path_1.default.join(__dirname, '..', '..', '..', 'data', 'data.db'),
+                type: 'mariadb',
+                database: app_config_1.default.db.database,
+                username: app_config_1.default.db.username,
+                password: app_config_1.default.db.password,
+                host: app_config_1.default.db.host,
+                port: app_config_1.default.db.port,
                 entities: [
                     Employee_1.Employee,
                     Client_1.Client,
